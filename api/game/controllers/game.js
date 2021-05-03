0d0850bc84b4ac5f82e451eb6f00f0ddec398b87
Bruno Matheus <bmatheusc@gmail.com>
2021-05-02 22:03:18 -0300

    Add crawler to get games data from gog

--- api/game/controllers/game.js
+++ api/game/controllers/game.js
@@ -1,8 +1,24 @@
 'use strict';
 
+const game = require("../services/game");
+
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+    populate: async (ctx) => {
+        console.log("Initializing");
+
+        const options = { 
+            sort: "popularity",
+            page: "1",
+            ...ctx.query
+        };
+
+        await game.populate(options);
+        
+        ctx.send({ ok: true });
+    }
+};
